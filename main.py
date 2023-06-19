b682becce43cf79de33edd6520f0097a1546ad77
Cristhian Castro <cristhiancastro001@gmail.com>
2023-06-19 01:11:26 -0500

    all API functions working

--- main.py
+++ main.py
@@ -62,4 +62,70 @@ def cantidad_filmaciones_dia(dia: str):
             return "Entered value is not valid." 
     else:
         return "Entered value is not valid."
+
+@app.get('/score_titulo/{titulo}')
+def score_titulo(titulo: str):
+    if type(titulo) == str:
+        titulo = titulo.lower().strip().replace(" ", "")
+        titulo = unidecode(titulo)  # delete accents
+        titulo = re.sub(r'[^\w\s]', '', titulo)  # delete special characters and punctuation marks      
+        df_grouped = df_movies.groupby("transformed_title")["popularity"].sum()
+        if titulo in df_grouped.index: # values of the grouped column are the new index in a grouped df
+            normal_index = (df_movies["transformed_title"] == titulo).idxmax() # index for non transformed and non grouped values
+            return {"Title": df_movies["title"][normal_index], "Year": df_movies["release_year"][normal_index].item(), "Popularity" : df_grouped[titulo].round(2).item()} 
+        else:
+            return "Entered value is not valid."
+    else:
+        return "Entered value is not valid."
+    
+@app.get('/votos_titulo/{titulo}')
+def votos_titulo(titulo: str):
+    if type(titulo) == str:
+        titulo = titulo.lower().strip().replace(" ", "")
+        titulo = unidecode(titulo)  # delete accents
+        titulo = re.sub(r'[^\w\s]', '', titulo)  # delete special characters and punctuation marks
+        df_grouped_total = df_movies.groupby("transformed_title")["vote_count"].sum()
+        df_grouped_average = df_movies.groupby("transformed_title")["vote_average"].mean()
+        if titulo in df_grouped_average.index: # values of the grouped column are the new index in a grouped df
+             if df_grouped_total[titulo] >= 2000:
+                normal_index = (df_movies["transformed_title"] == titulo).idxmax() # index for non transformed and non grouped values
+                return {"Title": df_movies["title"][normal_index], "Year": df_movies["release_year"][normal_index].item(), "Total Votes" : df_grouped_total[titulo].item(), "Average Vote" : df_grouped_average[titulo].item()}
+             else:
+                 return "Movie must have at least 2000 votes"
+        else:
+            return "Entered value is not valid."    
+    else:
+        return "Entered value is not valid."
+    
+@app.get('/get_actor/{nombre_actor}')
+def get_actor(nombre_actor):
+    if type(nombre_actor) == str:
+        nombre_actor = nombre_actor.lower().strip().replace(" ", "")
+        nombre_actor = unidecode(nombre_actor)  # delete accents
+        nombre_actor = re.sub(r'[^\w\s]', '', nombre_actor)  # delete special characters and punctuation marks
+        if nombre_actor in actor_financial["transformed_name"].unique():
+            index = (actor_financial["transformed_name"] == nombre_actor).idxmax()
+            return {'actor':actor_financial["name"][index], 'films':actor_financial["films"][index].item(), 'total return':actor_financial["total_return"][index].round(2).item(), 'average return':actor_financial["average_return"][index].item()}        
+        else:
+            return "Entered value is not valid."
+    return "Entered value is not valid."
+
+@app.get('/get_director/{nombre_director}')
+def get_director(nombre_director):
+    if type(nombre_director) == str:
+        nombre_director = nombre_director.lower().strip().replace(" ", "")
+        nombre_director = unidecode(nombre_director)  # delete accents
+        nombre_director = re.sub(r'[^\w\s]', '', nombre_director)  # delete special characters and punctuation marks
+        if nombre_director in director_financial["transformed_name"].unique():
+            index = (director_financial["transformed_name"] == nombre_director).idxmax()
+            movies_director = df_movies[df_movies["transformed_director"] == nombre_director]
+            movies_director = movies_director[["title","release_year", "return", "budget", "revenue"]]
+            return {
+                        'director':director_financial["name"][index], 
+                        'total return':director_financial["total_return"][index].item(), 
+                        'films': movies_director.to_dict(orient='records')
+                     }
+        else:
+            return "Entered value is not valid."
+    return "Entered value is not valid."
     
